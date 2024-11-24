81b53f88c5af4811dbcd7906440d4cf2338060de
bishal-dd <dhakalbishal930@gmail.com>
2024-11-24 13:21:09 +0600

    upgrade version

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
+  "/(.*)",
   "/home/sign-in(.*)",
   "/home/sign-up(.*)",
   "/home/contact(.*)",
