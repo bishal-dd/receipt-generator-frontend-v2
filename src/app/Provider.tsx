b7e2e0cf26d33757704c69cb2990fce92866d3d3
bishal-dd <dhakalbishal930@gmail.com>
2025-01-05 20:06:19 +0600

    add posthog and sentry

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -4,6 +4,16 @@ import { Loader } from "@/components/utils";
 import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
   return (
@@ -13,9 +23,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
           <Loader />
         </ClerkLoading>
         <ClerkLoaded>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
+          <PostHogProvider client={posthog}>
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </PostHogProvider>
         </ClerkLoaded>
       </ClerkProvider>
     </>
